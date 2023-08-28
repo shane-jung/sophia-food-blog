420d378fbe37008621b288cbe51f50c68cb3b35a
shane-jung <71043373+shane-jung@users.noreply.github.com>
2023-08-28 12:45:46 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "sophia-food-blog/",
+  base: "/sophia-food-blog/",
   plugins: [react()],
   build: {
     outDir: "dist",
